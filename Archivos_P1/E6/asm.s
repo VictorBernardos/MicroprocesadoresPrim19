fa28d9dbb78b770856033057c92f05fdd81e2690
VictorBernardos <victorbernardos1997@gmail.com>
2019-02-26 10:06:32 +0100

    Add files via upload

---
+++ Archivos_P1/E6/asm.s
@@ -0,0 +1,25 @@
+        AREA    RESET, DATA, READONLY
+        EXPORT  __Vectors
+__Vectors
+        DCD     0x10001800     ; Dirección de inicio del stack
+        DCD     Reset_Handler  ; Vector de reset
+
+        ALIGN
+        AREA    MI_PROGRAMA, CODE, READONLY
+        EXPORT  Reset_Handler
+			
+Reset_Handler
+        MOVS	R0,#0x0A
+		MOVS	R1,#0x0A
+		B		Compara
+Salto	
+		ADDS	R1,R1,#5
+		SUBS	R0,R0,#1
+Compara	
+		CMP		R0,#0x00	
+		BNE		Salto
+		
+forever
+        B       forever
+
+        END
