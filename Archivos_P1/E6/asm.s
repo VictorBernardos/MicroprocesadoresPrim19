2eba7b543e1fd819ca09f3223ec07fb612e4dbba
VictorBernardos <victorbernardos1997@gmail.com>
2019-03-01 16:45:22 +0100

    Update asm.s

--- Archivos_P1/E6/asm.s
+++ Archivos_P1/E6/asm.s
@@ -1,7 +1,7 @@
         AREA    RESET, DATA, READONLY
         EXPORT  __Vectors
 __Vectors
-        DCD     0x10001800     ; Dirección de inicio del stack
+        DCD     0x10001800     ; DirecciÃ³n de inicio del stack
         DCD     Reset_Handler  ; Vector de reset
 
         ALIGN
@@ -9,15 +9,15 @@ __Vectors
         EXPORT  Reset_Handler
 			
 Reset_Handler
-        MOVS	R0,#0x0A
-		MOVS	R1,#0x0A
-		B		Compara
+        	MOVS	R0,#0x0A
+		MOVS	R1,#0x00
+		B	Compara
 Salto	
 		ADDS	R1,R1,#5
 		SUBS	R0,R0,#1
 Compara	
-		CMP		R0,#0x00	
-		BNE		Salto
+		CMP	R0,#0x00	
+		BNE	Salto
 		
 forever
         B       forever
